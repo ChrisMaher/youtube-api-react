987e47d77cf015047519e4015de777abd304d647
Chris Maher <chrismaher.wit@gmail.com>
2016-02-28 22:51:16 +0000

    Added Youtube API package.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDN8aOydFxDDt0qmsPY9MckZQB-0MNSvTY';
+
+YTSearch({key : API_KEY, term: 'surfboards'}, function(data){
+
+  console.log(data);
+
+});
+
+const App = () => {
+    return (
+
+        <div>
+
+            <SearchBar />
+
+        </div>
+
+    )
+};
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
