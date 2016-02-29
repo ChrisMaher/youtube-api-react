e558de1ee30766f938cdabbf91e77934b3b96bfa
Chris Maher <chrismaher.wit@gmail.com>
2016-02-29 01:52:15 +0000

    Before Redux.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -18,22 +20,32 @@ class App extends Component {
             selectedVideo : null,
             videos: []};
 
+        this.videoSearch('mma');
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    }
+
+    videoSearch(term) {
+
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos : videos,
                 selectedVideo: videos[0]
             });
         });
+
     }
 
     render() {
 
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
-                <VideoList videos={this.state.videos} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         )
     }
