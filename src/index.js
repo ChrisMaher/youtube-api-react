5f10a9ac593f406f4bde1a30d0fe8327346a87c1
Chris Maher <chrismaher.wit@gmail.com>
2016-02-29 00:33:08 +0000

    Video Player Working.

--- src/index.js
+++ src/index.js
@@ -4,20 +4,26 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDN8aOydFxDDt0qmsPY9MckZQB-0MNSvTY';
-
-
 
 class App extends Component {
 
     constructor(props){
         super(props);
-        this.state = {videos: []};
+
+        this.state = {
+
+            selectedVideo : null,
+            videos: []};
+
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos : videos });
-
+            this.setState({
+                videos : videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -26,6 +32,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={this.state.videos} />
             </div>
         )
